136625436567f0578658eef41771ab2ce8f7b047
Yussuf Shaikh <yussufshaikh@gmail.com>
2021-12-09 22:15:14 +0530

    Revert "Refactor placement groups and others"

--- clients/instance/ibm-pi-cloud-instance.go
+++ clients/instance/ibm-pi-cloud-instance.go
@@ -1,12 +1,8 @@
 package instance
 
 import (
-	"context"
 	"fmt"
-
 	"github.com/IBM-Cloud/power-go-client/errors"
-	"github.com/IBM-Cloud/power-go-client/helpers"
-	"github.com/go-openapi/runtime"
 
 	"github.com/IBM-Cloud/power-go-client/ibmpisession"
 	"github.com/IBM-Cloud/power-go-client/power/client/p_cloud_instances"
@@ -16,60 +12,44 @@ import (
 // IBMPICloudInstanceClient ...
 type IBMPICloudInstanceClient struct {
 	session         *ibmpisession.IBMPISession
-	cloudInstanceID string
-	authInfo        runtime.ClientAuthInfoWriter
-	ctx             context.Context
+	powerinstanceid string
 }
 
 // NewIBMPICloudInstanceClient ...
-func NewIBMPICloudInstanceClient(ctx context.Context, sess *ibmpisession.IBMPISession, cloudInstanceID string) *IBMPICloudInstanceClient {
-	authInfo := ibmpisession.NewAuth(sess, cloudInstanceID)
+func NewIBMPICloudInstanceClient(sess *ibmpisession.IBMPISession, powerinstanceid string) *IBMPICloudInstanceClient {
 	return &IBMPICloudInstanceClient{
-		sess,
-		cloudInstanceID,
-		authInfo,
-		ctx,
+		session:         sess,
+		powerinstanceid: powerinstanceid,
 	}
 }
 
 // Get information about a cloud instance
-func (f *IBMPICloudInstanceClient) Get(id string) (*models.CloudInstance, error) {
-	params := p_cloud_instances.NewPcloudCloudinstancesGetParams().
-		WithContext(f.ctx).WithTimeout(helpers.PIGetTimeOut).
-		WithCloudInstanceID(id)
-	resp, err := f.session.Power.PCloudInstances.PcloudCloudinstancesGet(params, f.authInfo)
+func (f *IBMPICloudInstanceClient) Get(powerinstanceid string) (*models.CloudInstance, error) {
+	params := p_cloud_instances.NewPcloudCloudinstancesGetParams().WithCloudInstanceID(powerinstanceid)
+	resp, err := f.session.Power.PCloudInstances.PcloudCloudinstancesGet(params, ibmpisession.NewAuth(f.session, powerinstanceid))
 	if err != nil {
-		return nil, fmt.Errorf(errors.GetCloudInstanceOperationFailed, id, err)
-	}
-	if resp == nil || resp.Payload == nil {
-		return nil, fmt.Errorf("failed to Get Cloud Instance %s", id)
+		return nil, fmt.Errorf(errors.GetCloudInstanceOperationFailed, powerinstanceid, err)
 	}
 	return resp.Payload, nil
 }
 
 // Update a cloud instance
-func (f *IBMPICloudInstanceClient) Update(id string, body *models.CloudInstanceUpdate) (*models.CloudInstance, error) {
-	params := p_cloud_instances.NewPcloudCloudinstancesPutParams().
-		WithContext(f.ctx).WithTimeout(helpers.PIUpdateTimeOut).
-		WithCloudInstanceID(id).WithBody(body)
-	resp, err := f.session.Power.PCloudInstances.PcloudCloudinstancesPut(params, f.authInfo)
+func (f *IBMPICloudInstanceClient) Update(powerinstanceid string, updateparams *p_cloud_instances.PcloudCloudinstancesPutParams) (*models.CloudInstance, error) {
+	params := p_cloud_instances.NewPcloudCloudinstancesPutParamsWithTimeout(f.session.Timeout).WithCloudInstanceID(powerinstanceid).WithBody(updateparams.Body)
+	resp, err := f.session.Power.PCloudInstances.PcloudCloudinstancesPut(params, ibmpisession.NewAuth(f.session, powerinstanceid))
 	if err != nil {
-		return nil, fmt.Errorf(errors.UpdateCloudInstanceOperationFailed, id, err)
-	}
-	if resp == nil || resp.Payload == nil {
-		return nil, fmt.Errorf("failed to update the Cloud instance %s", id)
+		return nil, fmt.Errorf(errors.UpdateCloudInstanceOperationFailed, powerinstanceid, err)
+
 	}
 	return resp.Payload, nil
 }
 
 // Delete a Cloud instance
-func (f *IBMPICloudInstanceClient) Delete(id string) error {
-	params := p_cloud_instances.NewPcloudCloudinstancesDeleteParams().
-		WithContext(f.ctx).WithTimeout(helpers.PIDeleteTimeOut).
-		WithCloudInstanceID(id)
-	_, err := f.session.Power.PCloudInstances.PcloudCloudinstancesDelete(params, f.authInfo)
-	if err != nil {
-		return fmt.Errorf(errors.DeleteCloudInstanceOperationFailed, id, err)
+func (f *IBMPICloudInstanceClient) Delete(powerinstanceid string) (models.Object, error) {
+	params := p_cloud_instances.NewPcloudCloudinstancesDeleteParams().WithCloudInstanceID(powerinstanceid)
+	resp, err := f.session.Power.PCloudInstances.PcloudCloudinstancesDelete(params, ibmpisession.NewAuth(f.session, powerinstanceid))
+	if err != nil || resp.Payload == nil {
+		return nil, fmt.Errorf(errors.DeleteCloudInstanceOperationFailed, powerinstanceid, err)
 	}
-	return nil
+	return resp.Payload, nil
 }
