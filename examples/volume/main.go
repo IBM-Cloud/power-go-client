90a49343180560d025b5532fa73a5c8eea928c86
Yussuf Shaikh <yussuf.shaikh@ibm.com>
2021-12-09 23:34:03 +0530

    Clean up volume client
    Signed-off-by: Yussuf Shaikh <yussuf.shaikh@ibm.com>

--- examples/volume/main.go
+++ examples/volume/main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
+	"context"
 	"log"
-	"time"
 
 	v "github.com/IBM-Cloud/power-go-client/clients/instance"
 	ps "github.com/IBM-Cloud/power-go-client/ibmpisession"
+	"github.com/IBM-Cloud/power-go-client/power/models"
 )
 
 func main() {
-
 	//session Inputs
 	token := " < IAM TOKEN > "
 	region := " < REGION > "
+	zone := " < ZONE > "
 	accountID := " < ACCOUNT ID > "
+	//os.Setenv("IBMCLOUD_POWER_API_ENDPOINT", region + ".power-iaas.test.cloud.ibm.com")
 
 	// volume inputs
 	name := " < NAME OF THE volume > "
@@ -21,37 +23,42 @@ func main() {
 	size := 20.0
 	vtype := "tier1"
 	sharable := true
-	timeout := time.Duration(9000000000000000000)
 
-	session, err := ps.New(token, region, true, 9000000000000000000, accountID, region)
+	session, err := ps.New(token, region, true, 9000000000000000000, accountID, zone)
 	if err != nil {
 		log.Fatal(err)
 	}
-	powerClient := v.NewIBMPIVolumeClient(session, piID)
+	powerClient := v.NewIBMPIVolumeClient(context.Background(), session, piID)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	createRespOk, err := powerClient.Create(name, size, vtype, sharable, piID, timeout)
+	body := &models.CreateDataVolume{
+		Name:      &name,
+		Size:      &size,
+		DiskType:  vtype,
+		Shareable: &sharable,
+	}
+	createRespOk, err := powerClient.CreateVolume(body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Printf("***************[1]****************** %+v\n", createRespOk)
+	log.Printf("***************[1]****************** %+v\n", *createRespOk)
 
 	volumeID := *createRespOk.VolumeID
-	getResp, err := powerClient.Get(volumeID, piID, timeout)
+	getResp, err := powerClient.Get(volumeID)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("***************[2]****************** %+v \n", *getResp)
 
-	getallResp, err := powerClient.GetAll(volumeID, piID, timeout)
+	getallResp, err := powerClient.GetAll()
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("***************[2]****************** %+v \n", *getallResp)
 
-	err = powerClient.Delete(volumeID, piID, timeout)
+	err = powerClient.DeleteVolume(volumeID)
 	if err != nil {
 		log.Fatal(err)
 	}
