a90d5726c6d09ddf20b4a0133bace189ce3fc26c
Yussuf Shaikh <yussuf.shaikh@ibm.com>
2022-02-24 11:48:23 +0530

    Refresh token for every API call
    Signed-off-by: Yussuf Shaikh <yussuf.shaikh@ibm.com>

--- clients/instance/ibm-pi-tenant.go
+++ clients/instance/ibm-pi-tenant.go
@@ -27,7 +27,7 @@ func (f *IBMPITenantClient) Get(tenantid string) (*models.Tenant, error) {
 	params := p_cloud_tenants.NewPcloudTenantsGetParams().
 		WithContext(f.ctx).WithTimeout(helpers.PIGetTimeOut).
 		WithTenantID(tenantid)
-	resp, err := f.session.Power.PCloudTenants.PcloudTenantsGet(params, f.authInfo)
+	resp, err := f.session.Power.PCloudTenants.PcloudTenantsGet(params, f.session.AuthInfo(f.cloudInstanceID))
 	if err != nil {
 		return nil, fmt.Errorf("failed to get tenant %s with error %w", tenantid, err)
 	}
@@ -42,7 +42,7 @@ func (f *IBMPITenantClient) GetSelfTenant() (*models.Tenant, error) {
 	params := p_cloud_tenants.NewPcloudTenantsGetParams().
 		WithContext(f.ctx).WithTimeout(helpers.PIGetTimeOut).
 		WithTenantID(f.session.Options.UserAccount)
-	resp, err := f.session.Power.PCloudTenants.PcloudTenantsGet(params, f.authInfo)
+	resp, err := f.session.Power.PCloudTenants.PcloudTenantsGet(params, f.session.AuthInfo(f.cloudInstanceID))
 	if err != nil {
 		return nil, fmt.Errorf("failed to get self tenant with error %w", err)
 	}
