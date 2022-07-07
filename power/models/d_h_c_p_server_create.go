25561e52296d9aae6a092c2d193a1e47a1106a9a
Chase Austin <chase@ibm.com>
2022-07-07 08:30:33 -0500

    Update Service-Broker to v1.97.2

--- power/models/d_h_c_p_server_create.go
+++ power/models/d_h_c_p_server_create.go
@@ -17,6 +17,9 @@ import (
 // swagger:model DHCPServerCreate
 type DHCPServerCreate struct {
 
+	// Optional cidr for DHCP private network
+	Cidr *string `json:"cidr,omitempty"`
+
 	// Optional cloud connection uuid to connect with DHCP private network
 	CloudConnectionID *string `json:"cloudConnectionID,omitempty"`
 
