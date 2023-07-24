d4728299308f38edd16a8ddc36901ee9d9183039
powervs-ibm <powervs1@ibm.com>
2023-07-24 22:37:09 +0000

    Generated Swagger client from service-broker commit 7fff989ec23ee718aac55e1e410a15093a352233

--- power/models/network_create.go
+++ power/models/network_create.go
@@ -17,9 +17,20 @@ import (
 )
 
 // NetworkCreate network create
+// Example: {"accessConfig":"internal-only","cidr":"192.168.1.0/24","gateway":"192.168.1.1","ipAddressRanges":[{"endingIPAddress":"192.168.1.254","startingIPAddress":"192.168.1.2"}],"mtu":1450,"name":"sample-network","type":"vlan"}
 //
 // swagger:model NetworkCreate
 type NetworkCreate struct {
+
+	// Network communication configuration
+	//   * `internal-only` - network is only used for internal host communication
+	//   * `outbound-only` - network will be capable of egress traffic
+	//   * `bidirectional-static-route` - network will be capable of ingress and egress traffic via static routes
+	//   * `bidirectional-bgp` - network will be capable of ingress and egress traffic via bgp configuration
+	//   * `bidirectional-l2out` - network will be capable of ingress and egress traffic via l2out ACI configuration
+	//
+	// Enum: [internal-only outbound-only bidirectional-static-route bidirectional-bgp bidirectional-l2out]
+	AccessConfig string `json:"accessConfig,omitempty"`
 
 	// Network in CIDR notation (192.168.0.0/24)
 	Cidr string `json:"cidr,omitempty"`
@@ -36,20 +47,46 @@ type NetworkCreate struct {
 	// Enable MTU Jumbo Network
 	Jumbo bool `json:"jumbo,omitempty"`
 
+	// Maximum transmission unit
+	// Maximum: 9000
+	// Minimum: 1450
+	Mtu *int64 `json:"mtu,omitempty"`
+
 	// Network Name
-	Name string `json:"name,omitempty"`
+	Name *string `json:"name,omitempty"`
 
 	// Type of Network - 'vlan' (private network) 'pub-vlan' (public network)
 	// Required: true
-	// Enum: [vlan pub-vlan]
+	// Enum: [vlan pub-vlan dhcp-vlan]
 	Type *string `json:"type"`
+}
+
+func (m *NetworkCreate) UnmarshalJSON(b []byte) error {
+	type NetworkCreateAlias NetworkCreate
+	var t NetworkCreateAlias
+	if err := json.Unmarshal([]byte("{\"accessConfig\":\"internal-only\",\"mtu\":1450}"), &t); err != nil {
+		return err
+	}
+	if err := json.Unmarshal(b, &t); err != nil {
+		return err
+	}
+	*m = NetworkCreate(t)
+	return nil
 }
 
 // Validate validates this network create
 func (m *NetworkCreate) Validate(formats strfmt.Registry) error {
 	var res []error
 
+	if err := m.validateAccessConfig(formats); err != nil {
+		res = append(res, err)
+	}
+
 	if err := m.validateIPAddressRanges(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateMtu(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -60,6 +97,57 @@ func (m *NetworkCreate) Validate(formats strfmt.Registry) error {
 	if len(res) > 0 {
 		return errors.CompositeValidationError(res...)
 	}
+	return nil
+}
+
+var networkCreateTypeAccessConfigPropEnum []interface{}
+
+func init() {
+	var res []string
+	if err := json.Unmarshal([]byte(`["internal-only","outbound-only","bidirectional-static-route","bidirectional-bgp","bidirectional-l2out"]`), &res); err != nil {
+		panic(err)
+	}
+	for _, v := range res {
+		networkCreateTypeAccessConfigPropEnum = append(networkCreateTypeAccessConfigPropEnum, v)
+	}
+}
+
+const (
+
+	// NetworkCreateAccessConfigInternalDashOnly captures enum value "internal-only"
+	NetworkCreateAccessConfigInternalDashOnly string = "internal-only"
+
+	// NetworkCreateAccessConfigOutboundDashOnly captures enum value "outbound-only"
+	NetworkCreateAccessConfigOutboundDashOnly string = "outbound-only"
+
+	// NetworkCreateAccessConfigBidirectionalDashStaticDashRoute captures enum value "bidirectional-static-route"
+	NetworkCreateAccessConfigBidirectionalDashStaticDashRoute string = "bidirectional-static-route"
+
+	// NetworkCreateAccessConfigBidirectionalDashBgp captures enum value "bidirectional-bgp"
+	NetworkCreateAccessConfigBidirectionalDashBgp string = "bidirectional-bgp"
+
+	// NetworkCreateAccessConfigBidirectionalDashL2out captures enum value "bidirectional-l2out"
+	NetworkCreateAccessConfigBidirectionalDashL2out string = "bidirectional-l2out"
+)
+
+// prop value enum
+func (m *NetworkCreate) validateAccessConfigEnum(path, location string, value string) error {
+	if err := validate.EnumCase(path, location, value, networkCreateTypeAccessConfigPropEnum, true); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (m *NetworkCreate) validateAccessConfig(formats strfmt.Registry) error {
+	if swag.IsZero(m.AccessConfig) { // not required
+		return nil
+	}
+
+	// value enum
+	if err := m.validateAccessConfigEnum("accessConfig", "body", m.AccessConfig); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -89,11 +177,27 @@ func (m *NetworkCreate) validateIPAddressRanges(formats strfmt.Registry) error {
 	return nil
 }
 
+func (m *NetworkCreate) validateMtu(formats strfmt.Registry) error {
+	if swag.IsZero(m.Mtu) { // not required
+		return nil
+	}
+
+	if err := validate.MinimumInt("mtu", "body", *m.Mtu, 1450, false); err != nil {
+		return err
+	}
+
+	if err := validate.MaximumInt("mtu", "body", *m.Mtu, 9000, false); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 var networkCreateTypeTypePropEnum []interface{}
 
 func init() {
 	var res []string
-	if err := json.Unmarshal([]byte(`["vlan","pub-vlan"]`), &res); err != nil {
+	if err := json.Unmarshal([]byte(`["vlan","pub-vlan","dhcp-vlan"]`), &res); err != nil {
 		panic(err)
 	}
 	for _, v := range res {
@@ -108,6 +212,9 @@ const (
 
 	// NetworkCreateTypePubDashVlan captures enum value "pub-vlan"
 	NetworkCreateTypePubDashVlan string = "pub-vlan"
+
+	// NetworkCreateTypeDhcpDashVlan captures enum value "dhcp-vlan"
+	NetworkCreateTypeDhcpDashVlan string = "dhcp-vlan"
 )
 
 // prop value enum
