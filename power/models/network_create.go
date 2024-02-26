8a0eef4b894d026802708475802ee8d99ef4c725
powervs-ibm <powervs1@ibm.com>
2024-02-26 00:37:21 +0000

    Generated Swagger client from service-broker commit a00b4eb4d63b497b12f9b8c526beb783bf990cd3

--- power/models/network_create.go
+++ power/models/network_create.go
@@ -46,7 +46,6 @@ type NetworkCreate struct {
 	Mtu *int64 `json:"mtu,omitempty"`
 
 	// Network Name
-	// Max Length: 255
 	Name string `json:"name,omitempty"`
 
 	// Type of Network - 'vlan' (private network) 'pub-vlan' (public network) 'dhcp-vlan' (for satellite locations only)
@@ -68,10 +67,6 @@ func (m *NetworkCreate) Validate(formats strfmt.Registry) error {
 	}
 
 	if err := m.validateMtu(formats); err != nil {
-		res = append(res, err)
-	}
-
-	if err := m.validateName(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -138,18 +133,6 @@ func (m *NetworkCreate) validateMtu(formats strfmt.Registry) error {
 	}
 
 	if err := validate.MaximumInt("mtu", "body", *m.Mtu, 9000, false); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (m *NetworkCreate) validateName(formats strfmt.Registry) error {
-	if swag.IsZero(m.Name) { // not required
-		return nil
-	}
-
-	if err := validate.MaxLength("name", "body", m.Name, 255); err != nil {
 		return err
 	}
 
