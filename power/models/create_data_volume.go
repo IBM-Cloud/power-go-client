35f53122a35d96ef223ba69725118b407269a99d
powervs-ibm <137309855+powervs-ibm@users.noreply.github.com>
2024-06-26 10:32:10 -0500

    Generated Swagger client from service-broker commit f6e02b8a86dff9db756936ce068fe9c691b65bf0 (#417)

--- power/models/create_data_volume.go
+++ power/models/create_data_volume.go
@@ -46,6 +46,9 @@ type CreateDataVolume struct {
 	// Indicates if the volume should be replication enabled or not
 	ReplicationEnabled *bool `json:"replicationEnabled,omitempty"`
 
+	// List of replication site for volume replication
+	ReplicationSites []string `json:"replicationSites,omitempty"`
+
 	// Indicates if the volume is shareable between VMs
 	Shareable *bool `json:"shareable,omitempty"`
 
