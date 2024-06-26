35d98663159db65d1cbe731e8602b68c5543d693
powervs-ibm <powervs1@ibm.com>
2024-06-26 06:11:28 +0000

    Generated Swagger client from service-broker commit f6e02b8a86dff9db756936ce068fe9c691b65bf0

--- power/models/volume.go
+++ power/models/volume.go
@@ -80,6 +80,9 @@ type Volume struct {
 	// True if volume is replication enabled otherwise false
 	ReplicationEnabled *bool `json:"replicationEnabled,omitempty"`
 
+	// List of replication site for volume replication
+	ReplicationSites []string `json:"replicationSites,omitempty"`
+
 	// Replication status of a volume
 	ReplicationStatus string `json:"replicationStatus,omitempty"`
 
