482fa565f371a37bd53bcbf5cbcbecaf1341d5ce
powervs-ibm <137309855+powervs-ibm@users.noreply.github.com>
2025-03-11 13:43:15 -0500

    [U] Add Virtual Tier APIs, Add Software Tier field to VSN models (#564)

--- power/client/p_cloud_virtual_serial_number/p_cloud_virtual_serial_number_client.go
+++ power/client/p_cloud_virtual_serial_number/p_cloud_virtual_serial_number_client.go
@@ -72,6 +72,8 @@ type ClientService interface {
 
 	PcloudVirtualserialnumberPut(params *PcloudVirtualserialnumberPutParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*PcloudVirtualserialnumberPutOK, error)
 
+	PcloudVirtualserialnumberSoftwaretiersGetall(params *PcloudVirtualserialnumberSoftwaretiersGetallParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*PcloudVirtualserialnumberSoftwaretiersGetallOK, error)
+
 	SetTransport(transport runtime.ClientTransport)
 }
 
@@ -193,7 +195,7 @@ func (a *Client) PcloudPvminstancesVirtualserialnumberPost(params *PcloudPvminst
 }
 
 /*
-PcloudPvminstancesVirtualserialnumberPut updates description of a virtual serial number
+PcloudPvminstancesVirtualserialnumberPut updates a virtual serial number
 */
 func (a *Client) PcloudPvminstancesVirtualserialnumberPut(params *PcloudPvminstancesVirtualserialnumberPutParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*PcloudPvminstancesVirtualserialnumberPutOK, error) {
 	// TODO: Validate the params before sending
@@ -384,6 +386,45 @@ func (a *Client) PcloudVirtualserialnumberPut(params *PcloudVirtualserialnumberP
 	// unexpected success response
 	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
 	msg := fmt.Sprintf("unexpected success response for pcloud.virtualserialnumber.put: API contract not enforced by server. Client expected to get an error, but got: %T", result)
+	panic(msg)
+}
+
+/*
+PcloudVirtualserialnumberSoftwaretiersGetall lists supported software tiers i b mi licensing
+*/
+func (a *Client) PcloudVirtualserialnumberSoftwaretiersGetall(params *PcloudVirtualserialnumberSoftwaretiersGetallParams, authInfo runtime.ClientAuthInfoWriter, opts ...ClientOption) (*PcloudVirtualserialnumberSoftwaretiersGetallOK, error) {
+	// TODO: Validate the params before sending
+	if params == nil {
+		params = NewPcloudVirtualserialnumberSoftwaretiersGetallParams()
+	}
+	op := &runtime.ClientOperation{
+		ID:                 "pcloud.virtualserialnumber.softwaretiers.getall",
+		Method:             "GET",
+		PathPattern:        "/v1/virtual-serial-number/software-tiers",
+		ProducesMediaTypes: []string{"application/json"},
+		ConsumesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             params,
+		Reader:             &PcloudVirtualserialnumberSoftwaretiersGetallReader{formats: a.formats},
+		AuthInfo:           authInfo,
+		Context:            params.Context,
+		Client:             params.HTTPClient,
+	}
+	for _, opt := range opts {
+		opt(op)
+	}
+
+	result, err := a.transport.Submit(op)
+	if err != nil {
+		return nil, err
+	}
+	success, ok := result.(*PcloudVirtualserialnumberSoftwaretiersGetallOK)
+	if ok {
+		return success, nil
+	}
+	// unexpected success response
+	// safeguard: normally, absent a default response, unknown success responses return an error above: so this is a codegen issue
+	msg := fmt.Sprintf("unexpected success response for pcloud.virtualserialnumber.softwaretiers.getall: API contract not enforced by server. Client expected to get an error, but got: %T", result)
 	panic(msg)
 }
 
