b4af2a082f8628e78cc49da0dd0fc0e6132b31f9
powervs-ibm <137309855+powervs-ibm@users.noreply.github.com>
2025-03-11 13:43:15 -0500

    [U] Add Virtual Tier APIs, Add Software Tier field to VSN models (#564)

--- power/models/add_server_virtual_serial_number.go
+++ power/models/add_server_virtual_serial_number.go
@@ -25,6 +25,9 @@ type AddServerVirtualSerialNumber struct {
 	// Provide an existing reserved Virtual Serial Number or specify 'auto-assign' for auto generated Virtual Serial Number.
 	// Required: true
 	Serial *string `json:"serial"`
+
+	// software tier
+	SoftwareTier SoftwareTier `json:"softwareTier,omitempty"`
 }
 
 // Validate validates this add server virtual serial number
@@ -32,6 +35,10 @@ func (m *AddServerVirtualSerialNumber) Validate(formats strfmt.Registry) error {
 	var res []error
 
 	if err := m.validateSerial(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateSoftwareTier(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -50,8 +57,52 @@ func (m *AddServerVirtualSerialNumber) validateSerial(formats strfmt.Registry) e
 	return nil
 }
 
-// ContextValidate validates this add server virtual serial number based on context it is used
+func (m *AddServerVirtualSerialNumber) validateSoftwareTier(formats strfmt.Registry) error {
+	if swag.IsZero(m.SoftwareTier) { // not required
+		return nil
+	}
+
+	if err := m.SoftwareTier.Validate(formats); err != nil {
+		if ve, ok := err.(*errors.Validation); ok {
+			return ve.ValidateName("softwareTier")
+		} else if ce, ok := err.(*errors.CompositeError); ok {
+			return ce.ValidateName("softwareTier")
+		}
+		return err
+	}
+
+	return nil
+}
+
+// ContextValidate validate this add server virtual serial number based on the context it is used
 func (m *AddServerVirtualSerialNumber) ContextValidate(ctx context.Context, formats strfmt.Registry) error {
+	var res []error
+
+	if err := m.contextValidateSoftwareTier(ctx, formats); err != nil {
+		res = append(res, err)
+	}
+
+	if len(res) > 0 {
+		return errors.CompositeValidationError(res...)
+	}
+	return nil
+}
+
+func (m *AddServerVirtualSerialNumber) contextValidateSoftwareTier(ctx context.Context, formats strfmt.Registry) error {
+
+	if swag.IsZero(m.SoftwareTier) { // not required
+		return nil
+	}
+
+	if err := m.SoftwareTier.ContextValidate(ctx, formats); err != nil {
+		if ve, ok := err.(*errors.Validation); ok {
+			return ve.ValidateName("softwareTier")
+		} else if ce, ok := err.(*errors.CompositeError); ok {
+			return ce.ValidateName("softwareTier")
+		}
+		return err
+	}
+
 	return nil
 }
 
