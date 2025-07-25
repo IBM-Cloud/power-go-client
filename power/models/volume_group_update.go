a2b90bd601374fba54386d2dc0a3079086085bd0
powervs-ibm <137309855+powervs-ibm@users.noreply.github.com>
2025-07-25 09:39:58 -0500

    [A] DeleteDataVolume, VolumeGroupDelete: TargetCRN [U]NetworkPeer: string -> *string, UpdateVolume,VolumeAction,VolumeGroupCreate,VolumeGroupUpdate,VolumesDelete: TargetCRN from SB commit 11e762a6cfffbb913c8f775322b2845bf892e34b (#636)
    [A] DeleteDataVolume, VolumeGroupDelete: TargetCRN
    [U]NetworkPeer: string -> *string, UpdateVolume,VolumeAction,VolumeGroupCreate,VolumeGroupUpdate,VolumesDelete: TargetCRN

--- power/models/volume_group_update.go
+++ power/models/volume_group_update.go
@@ -22,6 +22,9 @@ type VolumeGroupUpdate struct {
 
 	// List of volume IDs to remove from the volume-group
 	RemoveVolumes []string `json:"removeVolumes"`
+
+	// Target CRN of the secondary workspace where the auxiliary data resides; optional; if specified, the auxiliary volumes for the primary volumes getting added/removed to/from the volume group will be automatically added/removed to/from the corresponding auxiliary consistency group; Added volumes will be automatically onboarded if not already onboarded prior to getting added to the auxiliary CG.
+	TargetCRN string `json:"targetCRN,omitempty"`
 }
 
 // Validate validates this volume group update
