940afaa672d602dcd325060d8c517b9726935850
Michael-Kadiayi <michael.kadiayi@ibm.com>
2026-07-21 10:46:53 -0500

    Remove Private gate for ws get & getall

--- clients/instance/ibm-pi-workspaces.go
+++ clients/instance/ibm-pi-workspaces.go
@@ -41,9 +41,6 @@ func NewIBMPIWorkspacesClient(ctx context.Context, sess *ibmpisession.IBMPISessi
 
 // Get a workspace
 func (f *IBMPIWorkspacesClient) Get(cloudInstanceID string) (*models.Workspace, error) {
-	if f.session.IsOnPrem() {
-		return nil, fmt.Errorf(helpers.NotOnPremSupported)
-	}
 	params := workspaces.NewV1WorkspacesGetParams().WithContext(f.ctx).WithTimeout(helpers.PIGetTimeOut).WithWorkspaceID(cloudInstanceID)
 	resp, err := f.session.Power.Workspaces.V1WorkspacesGet(params, f.session.AuthInfoV2())
 	if err != nil {
@@ -57,9 +54,6 @@ func (f *IBMPIWorkspacesClient) Get(cloudInstanceID string) (*models.Workspace, 
 
 // Get all workspaces
 func (f *IBMPIWorkspacesClient) GetAll() (*models.Workspaces, error) {
-	if f.session.IsOnPrem() {
-		return nil, fmt.Errorf(helpers.NotOnPremSupported)
-	}
 	params := workspaces.NewV1WorkspacesGetallParams().WithContext(f.ctx).WithTimeout(helpers.PIGetTimeOut)
 	resp, err := f.session.Power.Workspaces.V1WorkspacesGetall(params, f.session.AuthInfoV2())
 	if err != nil {
